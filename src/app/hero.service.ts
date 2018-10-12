76733354d3b41c5a095874397670266e6dc267f0
LeiHui <894556971@qq.com>
2018-10-12 15:23:05 +0800

    heroservice

---
+++ src/app/hero.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+import {Hero} from './hero';
+import {HEROES}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+}
